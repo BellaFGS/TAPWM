29b16438a84d3db393819fda18645d4d2b648a77
BellaFGS <ferreiraisabella1216@gmail.com>
2025-04-25 14:45:19 -0300

    Finalizado

--- Atividade7/script.js
+++ Atividade7/script.js
@@ -6,42 +6,53 @@ function calcularDados() {
     const resultadoTexto = document.getElementById("titulo");
 
     if (!isNaN(a) && !isNaN(b) && !isNaN(c) && a != 0 && b != 0 && c != 0) {
-        if (Math.abs(b - c) < a && a < (b + c) &&  
+        let classeFinal = "";
+
+        if (
+            Math.abs(b - c) < a && a < (b + c) &&  
             Math.abs(a - c) < b && b < (a + c) && 
-            Math.abs(a - b) < c && c < (a + b)) {
-
-            let tipo = "";
-
+            Math.abs(a - b) < c && c < (a + b)
+        ) {
             if (a === b && b === c) {
-                tipo = "equilatero";
+                classeFinal = "equilatero";
                 resultadoTexto.innerHTML = "Triângulo Equilátero";
             } else if (a === b || a === c || b === c) {
-                tipo = "isosceles";
+                classeFinal = "isosceles";
                 resultadoTexto.innerHTML = "Triângulo Isosceles";
             } else {
-                tipo = "escaleno";
+                classeFinal = "escaleno";
                 resultadoTexto.innerHTML = "Triângulo Escaleno";
             }
-
-            resultado.className = ""; 
-            resultado.classList.add(tipo);
-
         } else {
+            classeFinal = "invalido";
             resultadoTexto.innerHTML = "Triângulo Inválido";
-            resultado.className = "";
-            resultado.classList.add("invalido");
-        }   
+        }
+
+        resultado.className = classeFinal;
+        resultadoTexto.hidden = false;
+
     } else {
         alert("Valor inválido detectado, por favor digite números diferentes de 0.");
-        return;
     }
 }
+
+
 
 function limparFormulario() {
     document.getElementById("a").value = "";
     document.getElementById("b").value = "";
     document.getElementById("c").value = "";
-    resultado.className = "teacher";
-    resultadoTexto.innerHTML = "Triângulo";
-   
+
+    const resultado = document.getElementById("resultado");
+    const resultadoTexto = document.getElementById("titulo");
+
+    // Transição
+    resultado.style.opacity = 0;
+    setTimeout(() => {
+        resultado.className = "doctor"; // volta pra imagem inicial
+        resultado.style.opacity = 1;
+    }, 300);
+
+    resultadoTexto.hidden = true; 
+    resultadoTexto.innerHTML = "";
 }
