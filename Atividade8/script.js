95ef51c9c70ee35a339fd9f14f328017a7e112ad
BellaFGS <ferreiraisabella1216@gmail.com>
2025-05-06 10:34:18 -0300

    Exercicios funcionais

--- Atividade8/script.js
+++ Atividade8/script.js
@@ -34,8 +34,62 @@ function palindromo() {
 }
 
 function subConjunto() {
-    array1 = document.getElementById("array1").value.split(",").map(Number);
-    array2 = document.getElementById("array2").value.split(",").map(Number);
 
+    if (document.getElementById("palavra1").value === "" || document.getElementById("palavra2").value === "") {
+        alert("Por favor, preencha os dois campos.");
+        return;
+    }
+
+    palavra1 = document.getElementById("palavra1").value;
+    palavra2 = document.getElementById("palavra2").value;
+
+
+    palavra1 = palavra1.replace(/[^a-zA-Z]/g, "").toUpperCase(); //Ignora caracteres especiais e converte para minúsculas
+    palavra2 = palavra2.replace(/[^a-zA-Z]/g, "").toUpperCase(); //Ignora caracteres especiais e converte para minúsculas
+    var array1 = palavra1.split("");
+    var array2 = palavra2.split("");
+
+    let contador = {}; // Inicializa o contador
+
+    for (let letra of palavra1) {
+        if (contador[letra]) {
+            contador[letra]++;
+        }
+        else {
+            contador[letra] = 1;
+        }
+      }
+
+    var resultado = array2.every(function(letra) {
+        if (contador[letra] && contador[letra] > 0) {
+            contador[letra]--;
+            return true; // A letra existe e foi usada
+        } else {
+            return false; // A letra não existe ou já foi usada
+        }
+    });
     
+    if (resultado) {
+        document.getElementById("resultado").innerHTML = "(2)" + palavra2 + " ✔➡ (1)" + palavra1;
+    } else {
+        document.getElementById("resultado").innerHTML = "(2)" + palavra2 + " ❌➡ (1)" + palavra1;
+    }
+}
+
+function limparCampos() {
+    console.log("Limpar campos");
+
+    var ids = ["num1", "num2", "num3", "palindromo", "palavra1", "palavra2", "resultado"];
+    ids.forEach(function(id) {
+        var element = document.getElementById(id);
+        if (element) {
+            if (id === "resultado") {
+                element.innerHTML = "";
+            } else {
+                element.value = "";
+            }
+        } else {
+            console.warn(`Elemento com ID '${id}' não encontrado.`);
+        }
+    });
 }
