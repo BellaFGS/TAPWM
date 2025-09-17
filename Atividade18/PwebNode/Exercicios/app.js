9525a3bef5a8f39c4d146b3382f62a4e4f9812a5
BellaFGS <ferreiraisabella1216@gmail.com>
2025-09-17 18:07:04 -0300

    atividade18

---
+++ Atividade18/PwebNode/Exercicios/app.js
@@ -0,0 +1,24 @@
+let express = require('express');
+let texto = require('./modulo1');
+let app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req,res){
+    res.render("home/index");
+});
+app.get('/historia', function(req,res){
+    res.render("informacao/historia");
+});
+app.get('/cursos', function(req,res){
+    res.render("informacao/cursos");
+});
+app.get('/professores', function(req,res){
+    res.render("informacao/professores");
+});
+app.get('/formulario_adicionar_usuario', function(req,res){
+    res.render("admin/adicionar_usuario");
+});
+app.listen(3000, function(){
+    console.log(texto);
+});
